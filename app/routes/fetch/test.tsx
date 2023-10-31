fab73c3f4a251e505b16e9aaba467b487283f6dc
Alfredoes234 <alfie.rayner30@gmail.com>
2023-10-31 14:37:46 +0000

    bruh

---
+++ app/routes/fetch/test.tsx
@@ -0,0 +1,14 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+
+export default function Get() {
+    /* 
+    https://api.publicapis.org/
+    There is /entries, /random, /categories & /health
+    */
+    return(
+        <main>
+        </main>
+    )
+}
