6832e7b0adbcf76e38f7d03f2bd352f2be9de772
Alfredoes234 <alfie.rayner30@gmail.com>
2023-11-01 12:15:52 +0000

    changed nav

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -3,10 +3,10 @@ import { Link } from "@remix-run/react";
 export default function MainNav() {
     return(
         <nav className="flex justify-between m-5">
-            <li className="text-4xl font-bold"><Link to="/" prefetch="intent" >Logo</Link></li>
+            <h1 className="text-4xl font-bold"><Link to="/" prefetch="intent" >Logo</Link></h1>
             <div className="flex gap-5 text-2xl">
-                    <li><Link to="/cart" prefetch="intent" >cart</Link></li>
-                    <li><Link to="/login" prefetch="intent" >login</Link></li>
+                    <p><Link to="/cart" prefetch="intent" >cart</Link></p>
+                    <p><Link to="/login" prefetch="intent" >login</Link></p>
             </div>
         </nav>
     )
